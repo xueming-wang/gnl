71ec169529cfa259a7cb6f04f8f81a448b0c9c3a
xueming-wang <wxueming0614@hotmail.com>
2021-05-27 16:36:50 +0200

    ww

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: xuwang <xuwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/24 21:45:37 by xuwang            #+#    #+#             */
-/*   Updated: 2021/05/27 14:37:30 by xuwang           ###   ########.fr       */
+/*   Updated: 2021/05/27 16:34:02 by xuwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,31 @@
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
-
+int main(int ac, char **av)
+{
+    (void)ac;
+	int fd1 = open(av[1], O_RDONLY);
+	int fd2 = open(av[2], O_RDONLY);
+	int fd3 = open(av[3], O_RDONLY);
+    printf("第一轮\n\n");
+    
+	get_next_line(fd1, &av[1]);
+	printf("fd1: %s\n", av[1]);
+    get_next_line(fd2, &av[2]);
+	printf("fd2: %s\n", av[2]);
+    get_next_line(fd3, &av[3]);
+	printf("fd3: %s\n", av[3]);
+    
+     printf("\n\n第二轮\n\n");
+    
+	get_next_line(fd1, &av[1]);
+	printf("fd1: %s\n", av[1]);
+    get_next_line(fd2, &av[2]);
+	printf("fd1: %s\n", av[2]);
+    get_next_line(fd3, &av[3]);
+	printf("fd1: %s\n", av[3]);
+    
+}
 /*int main()
 {
     int fd1;
@@ -58,7 +82,7 @@
     return (0);
 }*/
 
-int main(int ac, char **av)
+/*int main(int ac, char **av)
 {
 	(void)ac;
 	
@@ -70,17 +94,6 @@ int main(int ac, char **av)
 		printf("%s\n", av[1]);
         i++;
     }
-	/*get_next_line(fd, &av[1]);
-	printf("%s\n", av[1]);
-    get_next_line(fd, &av[1]);
-	printf("%s\n", av[1]);
-    get_next_line(fd, &av[1]);
-	printf("%s\n", av[1]);
-    get_next_line(fd, &av[1]);
-	printf("%s\n", av[1]);
-    get_next_line(fd, &av[1]);
-	printf("%s\n", av[1]);*/
-	return 0;
-}
-
+		return 0;
+}*/
 
